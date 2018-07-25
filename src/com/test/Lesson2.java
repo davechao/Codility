3a2fe208a086090396bd89323e6426101869440f
Dave Chao <dave.chao@isuncloud.com>
2018-07-25 17:59:04 +0800

    adjust logic

--- src/com/test/Lesson2.java
+++ src/com/test/Lesson2.java
@@ -7,31 +7,37 @@ public class Lesson2 {
      *  Rotate an array to the right by a given number of steps.
      */
     public static void main(String[] args) {
-        int[] A = {1, 2, 3, 4};
-        int[] result = solution(A, 4);
+        int[] A = {3, 8, 9, 7, 6};
+        int[] result = solution(A, 3);
         for(int data : result) {
-            System.out.println(data);
+            System.out.print(data + " ");
         }
     }
 
 
     public static int[] solution(int[] A, int K) {
-        int[] result = new int[K];
-        int count = K;
-        while (count!=0) {
-            result = swap(A, K);
+
+        int[] result = new int[A.length];
+
+
+        if(A.length == 0) {
+            return result;
+        }
+
+        while (K!=0) {
+            result = swap(A, A.length);
             A = result;
-            count--;
+            K--;
         }
         return result;
     }
 
-    static int[] swap(int[] A, int K) {
-        int[] result = new int[K];
+    static int[] swap(int[] A, int length) {
+        int[] result = new int[length];
         for(int i=0;i<A.length-1;i++) {
             result[i+1] = A[i];
         }
-        result[0] = A[K-1];
+        result[0] = A[length-1];
         return result;
     }
 

d453aa0a5f8426d202a60b1d8e1f7255a51c6ad2
dave_chao <dave@coolbitx.com>
2018-07-27 21:42:07 +0800

    add Lesson6_1

---
+++ src/com/test/Lesson6_1.java
@@ -0,0 +1,25 @@
+package com.test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Lesson6_1 {
+
+    /*
+     *  Distinct
+     *  Compute number of distinct values in an array.
+     */
+    public static void main(String[] args) {
+        int[] A = {0};
+        System.out.println(solution(A));
+    }
+
+    public static int solution(int[] A) {
+        Set set = new HashSet();
+        for(int i=0;i<A.length;i++) {
+            set.add(A[i]);
+        }
+        return set.size();
+    }
+
+}
